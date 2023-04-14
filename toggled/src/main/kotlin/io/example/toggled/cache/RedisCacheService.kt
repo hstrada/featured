b39124de281fe8bdb133384fb58f9dde845ef41a
hstrada <6079947+hstrada@users.noreply.github.com>
2023-04-14 10:37:37 -0300

    refactor redis client

--- toggled/src/main/kotlin/io/example/toggled/cache/RedisCacheService.kt
+++ toggled/src/main/kotlin/io/example/toggled/cache/RedisCacheService.kt
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Service
 
 @Service
 class RedisCacheService(
-    private val reactiveRedisTemplate: RedisTemplate<String, String>,
+    private val redisTemplate: RedisTemplate<String, String>,
 ) : CacheService {
     override fun set(key: String, value: String) {
-        reactiveRedisTemplate.opsForValue().set(key, value)
+        redisTemplate.opsForValue().set(key, value)
     }
 
-    override fun get(key: String): String? = reactiveRedisTemplate.opsForValue().get(key)
+    override fun get(key: String): String? = redisTemplate.opsForValue().get(key)
 }
--- toggled/src/main/kotlin/io/example/toggled/config/CacheConfig.kt
+++ toggled/src/main/kotlin/io/example/toggled/config/CacheConfig.kt
@@ -1,16 +1,11 @@
 package io.example.toggled.config
 
-import io.example.toggled.feature.FeatureService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.ReactiveRedisTemplate
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.RedisSerializationContext.RedisSerializationContextBuilder
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.data.redis.core.RedisTemplate
 
 
 @Configuration
@@ -21,16 +16,10 @@ class CacheConfig {
         LettuceConnectionFactory(RedisStandaloneConfiguration("localhost", 6379))
 
     @Bean
-    fun reactiveRedisTemplate(
-        factory: ReactiveRedisConnectionFactory?
-    ): ReactiveRedisTemplate<String, FeatureService.Feature>? {
-        val keySerializer = StringRedisSerializer()
-        val valueSerializer: Jackson2JsonRedisSerializer<FeatureService.Feature> =
-            Jackson2JsonRedisSerializer(FeatureService.Feature::class.java)
-        val builder: RedisSerializationContextBuilder<String, FeatureService.Feature> =
-            RedisSerializationContext.newSerializationContext(keySerializer)
-        val context: RedisSerializationContext<String, FeatureService.Feature> = builder.value(valueSerializer).build()
-        return ReactiveRedisTemplate(factory!!, context)
+    fun redisTemplate(redisClient: RedisConnectionFactory?): RedisTemplate<String, String>? {
+        val template = RedisTemplate<String, String>()
+        template.setConnectionFactory(redisClient!!)
+        return template
     }
 
 }
