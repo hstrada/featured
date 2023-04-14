999961f9469a1d8427ebd2ca84c94bd3afb75887
hstrada <6079947+hstrada@users.noreply.github.com>
2023-04-14 09:50:14 -0300

    trying to improve some javadocs readbility

--- toggled/src/main/kotlin/io/example/toggled/cache/CacheService.kt
+++ toggled/src/main/kotlin/io/example/toggled/cache/CacheService.kt
@@ -1,7 +1,21 @@
 package io.example.toggled.cache
 
+/**
+ *
+ * Cache interface that will have the get on set methods
+ * to load and save cache data into service
+ */
 interface CacheService {
+    /**
+     * This method is used to save data into cache
+     * @param   key     key name that will be used to save data into cache
+     * @param   value   value that will be saved into key
+     */
     fun set(key: String, value: String)
 
+    /**
+     * This method is used to load data from cache
+     * @param   key     key name that will be used to search for data from cache
+     */
     fun get(key: String): String?
 }
